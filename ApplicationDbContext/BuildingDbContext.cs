4080de383fc402dadb2659a34697ccd78d7566bb
Farhang <farhang.ezziirani@gmail.com>
2025-10-11 00:44:56 +0330

    some jobs

--- ApplicationDbContext/BuildingDbContext.cs
+++ ApplicationDbContext/BuildingDbContext.cs
@@ -1,7 +1,9 @@
-using System.Data.Common;
 using BuldingManager.Configs;
+using BuldingManager.Domain;
 using BuldingManager.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
 
 namespace BuldingManager.ApplicationDbContext;
 
@@ -13,5 +15,34 @@ public partial class BuildingDbContext(DbContextOptions<BuildingDbContext> optio
            base.OnModelCreating(modelBuilder);
            modelBuilder.ApplyAllEntityConfigurations();
 
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                var method = typeof(BuildingDbContext)
+                    .GetMethod(nameof(SetSoftDeleteFilter), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                    ?.MakeGenericMethod(entityType.ClrType);
+
+                method?.Invoke(null, new object[] { modelBuilder });
+            }
+        }
+
+    }
+
+    public override int SaveChanges()
+    {
+        AuditHelper.ApplyAuditInfo(ChangeTracker);
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        AuditHelper.ApplyAuditInfo(ChangeTracker);
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    private static void SetSoftDeleteFilter<T>(ModelBuilder builder) where T : BaseEntity
+    {
+        builder.Entity<T>().HasQueryFilter(x => !x.IsDeleted);
     }
 }
---
+++ Controllers/AttributeController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using BuldingManager.Dto.Attribute;
+using BuldingManager.Services.Attribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuldingManager.Controllers
+{
+    [Controller]
+    [Route("api/v1/[controller]")]
+    public class AttributeController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAttributeService _service;
+        public AttributeController(IMapper mapper,IAttributeService service)
+        {
+            _mapper = mapper;
+            _service= service;  
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AttributeGetDto>>> GetAttribute()
+        {
+            var attributes =  await _service.GetAttributes();
+            if (attributes==null)
+            {
+                return NotFound();  
+            }
+            return Ok(attributes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AttributeGetDto>> GetAttribute(int id)
+        {
+            var attribute = await _service.GetAttribute(id);
+            if (attribute==null)
+            {
+                return NotFound();  
+            }
+            return Ok(attribute);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAttribute([FromBody] attributeCreateDto attribute)
+        {
+            await _service.CreateAttribute(attribute);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAttribute(int id,[FromBody] attributeCreateDto attribute)
+        {
+            await _service.UpdateAttribute(id, attribute);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttribute(int id)
+        {
+            await _service.DeleteAttribute(id);
+            return Ok();
+        }
+
+
+    }
+}
